95d6ddf8126d0f5ffb113f99174a321f6d86e7b7
ChangShuo <changshuo@bupt.edu.cn>
2022-03-24 15:48:58 +0800

    update _base_ config files

--- configs/_base_/datasets/ap-deepsig-201610A.py
+++ configs/_base_/datasets/ap-deepsig-201610A.py
@@ -37,7 +37,7 @@ data = dict(
             dict(type='EvaluateModulationPrediction', )
         ],
         save=[
-          dict(type='SaveModulationPrediction',)
+            dict(type='SaveModulationPrediction', )
         ],
     ),
 )
