9e0aa44f0cf9a9ca4acbebd2116d6fa42fa4c243
ChangShuo <changshuo@bupt.edu.cn>
2022-03-24 15:46:48 +0800

    update CNN3 config files

---
+++ configs/cnn3/cnn3_iq-deepsig-201801A.py
@@ -0,0 +1,38 @@
+_base_ = [
+    '../_base_/datasets/iq-deepsig-201801A.py',
+    '../_base_/default_runtime.py'
+]
+
+# Model
+model = dict(
+    type='DNN',
+    method_name='CNN3',
+    backbone=dict(
+        type='CNNNet',
+        depth=3,
+        in_channels=1,
+        out_indices=(2,),
+        avg_pool=(1, 8),
+    ),
+    classifier_head=dict(
+        type='AMCHead',
+        num_classes=24,
+        in_features=10240,
+        out_features=256,
+        loss_cls=dict(
+            type='CrossEntropyLoss',
+            loss_weight=1.0,
+        ),
+    ),
+)
+
+train_cfg = dict()
+test_cfg = dict()
+
+total_epochs = 400
+
+# Optimizer
+optimizer = dict(type='Adam')
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(policy='fixed')
