df344877a9496f51564efab4a6d7cb4a74c76fa5
ChangShuo <changshuo@bupt.edu.cn>
2022-03-24 15:48:44 +0800

    update CNN4 config files

---
+++ configs/cnn4/cnn4_iq-deepsig-201610A.py
@@ -0,0 +1,37 @@
+_base_ = [
+    '../_base_/datasets/iq-deepsig-201610A.py',
+    '../_base_/default_runtime.py'
+]
+
+# Model
+model = dict(
+    type='DNN',
+    method_name='CNN4',
+    backbone=dict(
+        type='CNNNet',
+        depth=4,
+        in_channels=1,
+        out_indices=(3,),
+    ),
+    classifier_head=dict(
+        type='AMCHead',
+        num_classes=11,
+        in_features=10880,
+        out_features=128,
+        loss_cls=dict(
+            type='CrossEntropyLoss',
+            loss_weight=1.0,
+        ),
+    ),
+)
+
+train_cfg = dict()
+test_cfg = dict()
+
+total_epochs = 400
+
+# Optimizer
+optimizer = dict(type='Adam')
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(policy='fixed')
