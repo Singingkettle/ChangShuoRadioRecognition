54b4fcc9a952ee0b86b024cf444b23393b11a2e0
ChangShuo <changshuo@bupt.edu.cn>
2023-04-25 11:20:17 +0800

    add abl fastmldnn configs about different fea merge strategies

---
+++ configs/fastmldnn/fastmldnn_abl-merge-logsumexp_iq-ap-channel-deepsig201610A.py
@@ -0,0 +1,42 @@
+_base_ = [
+    './schedule.py',
+    './default_runtime.py',
+    './data_iq-ap-channel-deepsig201610A.py'
+]
+
+in_size = 100
+out_size = 288
+num_classes = 11
+model = dict(
+    type='FastMLDNN',
+    backbone=dict(
+        type='FMLNet',
+        input_size=in_size,
+        merge='logsumexp',
+    ),
+    classifier_head=dict(
+        type='AMCHead',
+        num_classes=11,
+        in_size=in_size,
+        out_size=out_size,
+        loss_cls=dict(
+            type='CrossEntropyLoss',
+            loss_weight=1
+        ),
+        init_cfg=[
+            dict(type='TruncNormal', layer='Linear', std=0.02, bias=0.),
+        ],
+    )
+)
+
+runner = dict(type='EpochBasedRunner', max_epochs=400)
+# Optimizer
+# for flops calculation
+optimizer = dict(type='Adam', lr=0.00044)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    gamma=0.3,
+    step=[800, 1200]
+)
