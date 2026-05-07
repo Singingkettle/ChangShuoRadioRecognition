001d49bd114bc92b0cf1e491e760bbf359e58e2c
ChangShuo <changshuo@bupt.edu.cn>
2026-05-07 22:00:24 +0800

    chore: clean specstory files and normalize mldnn schedule

--- configs/mldnn/schedules.py
+++ configs/mldnn/schedules.py
@@ -1,9 +1,7 @@
 # schedule settings
-optim_wrapper = dict(
-    optimizer=dict(type='Adam', lr=0.0004)
-)
+optim_wrapper = dict(optimizer=dict(type='Adam', lr=0.0004))
 
-train_cfg = dict(by_epoch=True, max_epochs=400, val_interval=1)  # train 5 epochs
+train_cfg = dict(by_epoch=True, max_epochs=400, val_interval=1)
 val_cfg = dict()
 test_cfg = dict()
-randomness=dict(seed=3407)
+randomness = dict(seed=3407)
