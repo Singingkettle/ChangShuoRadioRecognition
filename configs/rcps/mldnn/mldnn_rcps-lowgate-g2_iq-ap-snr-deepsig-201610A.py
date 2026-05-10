1decad1a1c9effe1b245b7edebc66130e13a10c7
ChangShuo <changshuo@bupt.edu.cn>
2026-05-11 04:22:35 +0800

    Add conservative LowGate tuning configs

---
+++ configs/rcps/mldnn/mldnn_rcps-lowgate-g2_iq-ap-snr-deepsig-201610A.py
@@ -0,0 +1,13 @@
+_base_ = ['../_base_/models/mldnn_iq-ap-snr-deepsig-201610A.py']
+
+work_dir = '/home/citybuster/Data/RCPS/work_dirs/amc/deepsig201610A/mldnn_rcps-lowgate-g2'
+
+_rcps_loss = dict(
+    type='RCPSCrossEntropyLoss',
+    reliability_key='snr_db',
+    reliability_map=dict(type='linear', min=-20, max=18),
+    epsilon=dict(type='low_reliability_power', max=0.7, gamma=2.0, cutoff=0.2631578947),
+    base=dict(type='uniform'),
+    sample_weight=dict(type='none'),
+    loss_weight=1)
+model = dict(head=dict(loss_amc_merge=_rcps_loss, loss_amc_ap=_rcps_loss, loss_amc_iq=_rcps_loss))
