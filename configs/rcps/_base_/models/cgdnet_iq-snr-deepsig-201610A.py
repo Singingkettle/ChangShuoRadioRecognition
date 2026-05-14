7faf3d4ac5db20372ee9853b31c92a6a0e5bb926
ChangShuo <changshuo@bupt.edu.cn>
2026-05-14 16:58:25 +0800

    Restore CGDNet GRU initializer for parity

--- configs/rcps/_base_/models/cgdnet_iq-snr-deepsig-201610A.py
+++ configs/rcps/_base_/models/cgdnet_iq-snr-deepsig-201610A.py
@@ -25,6 +25,7 @@ model = dict(
         num_classes=11,
         init_cfg=[
             dict(type='Kaiming', layer='Linear', mode='fan_in'),
+            dict(type='RNN', layer='GRU', gain=1),
             dict(
                 type='Xavier',
                 layer='Conv2d',
