a9f8438707ff5574209d9d2278a4d210a0055097
ChangShuo <changshuo@bupt.edu.cn>
2026-05-18 16:58:55 +0800

    Add hybrid-projected DPC PETCGDNN config

---
+++ configs/rcps/dpc/petcgdnn_dpc-hybrid_iq-snr-deepsig-201610A.py
@@ -0,0 +1,21 @@
+_base_ = ['../_base_/models/petcgdnn_iq-snr-deepsig-201610A.py']
+
+work_dir = '/home/citybuster/Data/RCPS/work_dirs/dpc_smoke/amc/deepsig201610A/petcgdnn_dpc-hybrid'
+method_name = 'dpc_rcps_hybrid_projection'
+
+model = dict(
+    head=dict(
+        loss=dict(
+            type='RCPSCrossEntropyLoss',
+            reliability_key='snr',
+            reliability_map=dict(type='linear', min=-20, max=18),
+            epsilon=dict(type='retention_power', max=1.0, gamma=1.0, retain_min=0.75),
+            base=dict(
+                type='sample_posterior',
+                source='/home/citybuster/Data/RCPS/work_dirs/dpc_teacher_posteriors/deepsig201610A/petcgdnn_hard-ce_seed2026_train.npz',
+                laplace=1e-4,
+                temperature=1.2,
+                prior_blend=0.75,
+                prior=dict(type='uniform')),
+            sample_weight=dict(type='none'),
+            loss_weight=1.0)))
