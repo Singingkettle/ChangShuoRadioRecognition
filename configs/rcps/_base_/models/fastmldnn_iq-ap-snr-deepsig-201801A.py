5af00382c0777cd8f2e8eebc27c7cead45f8f355
ChangShuo <changshuo@bupt.edu.cn>
2026-05-22 06:48:16 +0800

    configs(rcps): add FastMLDNN 2018A baseline gate

---
+++ configs/rcps/_base_/models/fastmldnn_iq-ap-snr-deepsig-201801A.py
@@ -0,0 +1,37 @@
+_base_ = [
+    "../../../fastmldnn/iq-ap-deepsig201801A.py",
+    "../../../_base_/schedules/amc.py",
+    "../../../_base_/runtimes/amc.py",
+]
+
+data_root = "/home/citybuster/Data/WirelessRadio/data/ModulationClassification/DeepSig/RadioML.2018.01A"
+
+snr_train_pipeline = [
+    dict(type="MLDNNIQToAP"),
+    dict(type="Reshape", shapes=dict(iq=[2, 1024])),
+    dict(type="Reshape", shapes=dict(ap=[2, 1024])),
+    dict(
+        type="PackInputs",
+        input_key=["iq", "ap"],
+        meta_keys=("sample_idx", "snr", "snr_label", "modulation")),
+]
+
+snr_pipeline = [
+    dict(type="MLDNNIQToAP"),
+    dict(type="Reshape", shapes=dict(iq=[2, 1024])),
+    dict(type="Reshape", shapes=dict(ap=[2, 1024])),
+    dict(
+        type="PackInputs",
+        input_key=["iq", "ap"],
+        meta_keys=("sample_idx", "snr", "snr_label", "modulation")),
+]
+
+train_dataloader = dict(dataset=dict(data_root=data_root, pipeline=snr_train_pipeline))
+val_dataloader = dict(dataset=dict(data_root=data_root, pipeline=snr_pipeline))
+test_dataloader = dict(dataset=dict(data_root=data_root, pipeline=snr_pipeline))
+
+model = dict(
+    type="SignalClassifier",
+    backbone=dict(type="FastMLDNN", num_classes=24),
+    head=dict(type="ClsHead", loss=dict(type="CrossEntropyLoss", loss_weight=1.0)),
+)
---
+++ configs/rcps/fastmldnn/fastmldnn_hard-ce_iq-ap-snr-deepsig-201801A.py
@@ -0,0 +1,6 @@
+_base_ = ["../_base_/models/fastmldnn_iq-ap-snr-deepsig-201801A.py"]
+
+work_dir = "/home/citybuster/Data/RCPS/work_dirs/baseline_gate_2018A/amc/deepsig201801A/fastmldnn_hard-ce"
+method_name = "hard_ce"
+
+experiment_note = "Baseline gate for FastMLDNN on RadioML2018.01A with SNR metadata for RCPS reliability analysis."
