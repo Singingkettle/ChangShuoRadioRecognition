e3b96f41d1ff214d28a0b477de991c9c0d6699b8
ChangShuo <changshuo@bupt.edu.cn>
2026-05-22 09:13:05 +0800

    configs(rcps): add MCLDNN 2018A baseline gate

---
+++ configs/rcps/mcldnn/mcldnn_hard-ce_iq-snr-deepsig-201801A.py
@@ -0,0 +1,4 @@
+_base_ = ['../_base_/models/mcldnn_iq-snr-deepsig-201801A.py']
+
+work_dir = '/home/citybuster/Data/RCPS/work_dirs/baseline_gate_2018A_mcldnn/amc/deepsig201801A/mcldnn_hard-ce'
+method_name = 'hard_ce_baseline_gate_2018A_mcldnn'
